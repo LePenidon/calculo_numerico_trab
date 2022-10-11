b8014ff92b831025deee85b97ab9cc664592c883
lepenidon <penido1000@gmail.com>
2022-10-11 20:42:29 -0300

    day one

--- minimosquadrados.m
+++ minimosquadrados.m
@@ -1,25 +1,33 @@
-x = csvread('SeO4.csv')
+x = csvread('SeO4.csv');
 coluna1 = x(:,1);
 coluna2 = x(:,2);
-plot(coluna1, coluna2, "m")
+% plot(coluna1, coluna2, "m")
 
 
-##function w = pesos(x, teta)
-##
-##  n = lenght(x);
-##
-##  for i = 1:n
-##    t = (x - x(i))/teta;
-##    psi = e^(-(t^2));
-##    w(i) = psi;
-##  endfor
-##end
-##
-##
-##function a  = mmqp(x, y, w, k)
-##  n = lenght(x);
-##  W = diag(w);
-##  X = vander(X);
-##  X = X(:, n - k : n);
-##  a = ((X' * W * X) \ (X' * W * y'))';
-##end
+function w = pesos(x, sigma)
+
+  n = size(x)(1);
+
+  for i = 1:n
+    t = abs(x - x(i)/sigma;
+    psi = exp(-(t.^2));
+    w = psi;
+  endfor
+end
+
+
+function a  = mmqp(x, y, w, k)
+  n = size(x)(1);
+  W = diag(w);
+  X = vander(x);
+  X = X(:, n - k : n);
+
+  a = ((X' * W * X) \ (X' * W * y))';
+end
+
+
+teste = mmqp(coluna1,coluna2,pesos(coluna1, 100),4);
+
+f = polyval(teste, coluna1);
+
+plot(coluna1, coluna2, "o", coluna1, f, '-')
