59145c6a88660ced6eaf12a5630a8e732b94d27d
lepenidon <penido1000@gmail.com>
2022-10-17 15:25:52 -0300

    intervalos

--- minimosquadrados.m
+++ minimosquadrados.m
@@ -2,22 +2,49 @@ x = csvread('SeO4.csv');
 coluna1 = x(:,1);
 coluna2 = x(:,2);
 
+% ===============================================================================
+
+col1_01 = x(1:200,1);
+col2_01 = x(1:200,2);
+
+
 c = 12.63;
-sigma = 0.06;
+sigma = 0.01;
 k = 3;
 
-pesos = exp(-((abs(coluna1-c)/sigma).^2));
-tam_coluna1 = length(coluna1);
+pesos = exp(-((abs(col1_01-c)/sigma).^2));
+tam_coluna1 = length(col1_01);
 W = diag(pesos);
-X = vander(coluna1);
+X = vander(col1_01);
 X = X(:, tam_coluna1 - k : tam_coluna1);
 
-aprox = ((X' * W * X) \ (X' * W * coluna2));
+aprox = ((X' * W * X) \ (X' * W * col2_01));
 
-f = X*aprox;
+f_1 = X*aprox;
 
-aprox
+% ===============================================================================
+
+col1_02 = x(200:250,1);
+col2_02 = x(200:250,2);
+
+
+c = 12.67;
+sigma = 0.01;
+k = 3;
+
+pesos = exp(-((abs(col1_02-c)/sigma).^2));
+tam_coluna1 = length(col1_02);
+W = diag(pesos);
+X = vander(col1_02);
+X = X(:, tam_coluna1 - k : tam_coluna1);
+
+aprox = ((X' * W * X) \ (X' * W * col2_02));
+
+f_2 = X*aprox;
+
+% ==============================================================================
 
 hold 
-plot(coluna1, coluna2, "o");
-plot(coluna1, f, '-');
+plot(coluna1, coluna2, "-");
+plot(col1_01, f_1, '-');
+plot(col1_02, f_2, '-');
