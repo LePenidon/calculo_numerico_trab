1d5e3bd3c9b46f08bd3b0becc7f1ee51112ad043
lepenidon <penido1000@gmail.com>
2022-10-18 19:48:59 -0300

    todos intervalos

--- minimosquadrados.m
+++ minimosquadrados.m
@@ -8,11 +8,11 @@ coluna2 = x(:,2);
 
 % ===============================================================================
 
-col1_01 = x(1:150,1);
-col2_01 = x(1:150,2);
+col1_01 = x(1:140,1);
+col2_01 = x(1:140,2);
 
-c = col1_01(75);
-sigma = 0.016;
+c = col1_01(70);
+sigma = 0.001;
 k = 3;
 
 pesos = exp(-((abs(col1_01-c)/sigma).^2));
@@ -21,35 +21,72 @@ W = diag(pesos);
 X = vander(col1_01);
 X = X(:, tam_coluna1 - k : tam_coluna1);
 
-aprox = ((X' * W * X) \ (X' * W * col2_01));
-% aprox = flipud(aprox);
+aprox = ((X' * W * X) \ (X' * W * col2_01))
 
 f_1 = X*aprox;
-% f_1 = polyval(aprox,col1_01);
 
 % ===============================================================================
 
-% col1_02 = x(150:250,1);
-% col2_02 = x(150:250,2);
+col1_02 = x(141:230,1);
+col2_02 = x(141:230,2);
 
-% c = col1_02(55);
-% sigma = 0.016;
-% k = 3;
+c = col1_02(90);
+sigma = 0.001;
+k = 2;
 
-% pesos = exp(-((abs(col1_02-c)/sigma).^2));
-% tam_coluna1 = length(col1_02);
-% W = diag(pesos);
-% X = vander(col1_02);
-% X = X(:, tam_coluna1 - k : tam_coluna1);
+pesos = exp(-((abs(col1_02-c)/sigma).^2));
+tam_coluna1 = length(col1_02);
+W = diag(pesos);
+X = vander(col1_02);
+X = X(:, tam_coluna1 - k : tam_coluna1);
 
-% aprox = ((X' * W * X) \ (X' * W * col2_02));
-% % aprox = flipud(aprox);
+aprox = ((X' * W * X) \ (X' * W * col2_02));
 
-% f_2 = polyval(aprox,col1_02);
+f_2 = X*aprox;
+
+% ===============================================================================
+
+col1_03 = x(231:280,1);
+col2_03 = x(231:280,2);
+
+c = col1_03(35);
+sigma = 0.001;
+k = 2;
+
+pesos = exp(-((abs(col1_03-c)/sigma).^2));
+tam_coluna1 = length(col1_03);
+W = diag(pesos);
+X = vander(col1_03);
+X = X(:, tam_coluna1 - k : tam_coluna1);
+
+aprox = ((X' * W * X) \ (X' * W * col2_03));
+
+f_3 = X*aprox;
+
+% ===============================================================================
+
+col1_04 = x(281:450,1);
+col2_04 = x(281:450,2);
+
+c = col1_04(135);
+sigma = 0.00001;
+k = 2;
+
+pesos = exp(-((abs(col1_04-c)/sigma).^2));
+tam_coluna1 = length(col1_04);
+W = diag(pesos);
+X = vander(col1_04);
+X = X(:, tam_coluna1 - k : tam_coluna1);
+
+aprox = ((X' * W * X) \ (X' * W * col2_04));
+
+f_4 = X*aprox;
 
 % ==============================================================================
 
 hold 
-plot(coluna1, coluna2, "-");
+plot(coluna1, coluna2, "");
 plot(col1_01, f_1, '-', 'LineWidth', 3);
-% plot(col1_02, f_2, '-', 'LineWidth', 3);
+plot(col1_02, f_2, '-', 'LineWidth', 3);
+plot(col1_03, f_3, '-', 'LineWidth', 3);
+plot(col1_04, f_4, '-', 'LineWidth', 3);
