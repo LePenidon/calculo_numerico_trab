14dfaa6fa4fa268cbbf23c4164f0d0fffc332a64
lepenidon <penido1000@gmail.com>
2022-10-20 20:26:14 -0300

    aeeeeeeeeeee

--- minimosquadrados.m
+++ minimosquadrados.m
@@ -16,7 +16,7 @@ col1_01 = col1_01 - 12;
 col2_01 = x(1:140,2);
 
 c_1 = col1_01(70);
-sigma_1 = 0.01;
+sigma_1 = 0.1;
 
 pesos = exp(-((abs(col1_01-c_1)/sigma_1).^2));
 tam_coluna1 = length(col1_01);
@@ -37,7 +37,7 @@ col1_02 = col1_02 - 12;
 col2_02 = x(141:230,2);
 
 c_2 = col1_02(40);
-sigma_2 = 0.009;
+sigma_2 = 0.1;
 
 pesos = exp(-((abs(col1_02-c_2)/sigma_2).^2));
 tam_coluna1 = length(col1_02);
@@ -57,7 +57,7 @@ col1_03 = col1_03 - 12;
 col2_03 = x(231:290,2);
 
 c_3 = col1_03(60);
-sigma_3 = 0.09;
+sigma_3 = 0.1;
 
 pesos = exp(-((abs(col1_03-c_3)/sigma_3).^2));
 tam_coluna1 = length(col1_03);
@@ -78,7 +78,7 @@ col1_04 = col1_04 - 12;
 col2_04 = x(290:330,2);
 
 c_4 = col1_04(20);
-sigma_4 = 0.01;
+sigma_4 = 0.1;
 
 pesos = exp(-((abs(col1_04-c_4)/sigma_4).^2));
 tam_coluna1 = length(col1_04);
@@ -99,7 +99,7 @@ col1_05 = col1_05 - 12;
 col2_05 = x(330:450,2);
 
 c_5 = col1_05(60);
-sigma_5 = 0.01;
+sigma_5 = 0.1;
 
 pesos = exp(-((abs(col1_05-c_5)/sigma_4).^2));
 tam_coluna1 = length(col1_05);
@@ -114,22 +114,24 @@ f_5 = X*aprox;
 % ==============================================================================
 
 hold on
-plot(coluna1, coluna2, 'DisplayName','Se04', 'LineWidth', 1);
+plot(coluna1, coluna2, 'DisplayName','Se04', 'LineWidth', 2);
 
 lgd_01 = strcat('c: ', num2str(c_1+12),"; ", "sigma: ", num2str(sigma_1));
-plot(coluna1(1:140), f_1, ':', 'LineWidth', 3, 'DisplayName',lgd_01);
+plot(coluna1(1:140), f_1, ':g', 'LineWidth', 3, 'DisplayName',lgd_01);
 
 lgd_02 = strcat('c: ', num2str(c_2+12),"; ", "sigma: ", num2str(sigma_2));
-plot(coluna1(141:230), f_2, ':', 'LineWidth', 3, 'DisplayName',lgd_02);
+plot(coluna1(141:230), f_2, ':y', 'LineWidth', 3, 'DisplayName',lgd_02);
 
 lgd_03 = strcat('c: ', num2str(c_3+12),"; ", "sigma: ", num2str(sigma_3));
-plot(coluna1(231:290), f_3, ':', 'LineWidth', 3, 'DisplayName',lgd_03);
+plot(coluna1(231:290), f_3, ':r', 'LineWidth', 3, 'DisplayName',lgd_03);
 
 lgd_04 = strcat('c: ', num2str(c_4+12),"; ", "sigma: ", num2str(sigma_4));
-plot(coluna1(290:330), f_4, ':', 'LineWidth', 3, 'DisplayName',lgd_04);
+plot(coluna1(290:330), f_4, ':c', 'LineWidth', 3, 'DisplayName',lgd_04);
 
 lgd_05 = strcat('c: ', num2str(c_5+12),"; ", "sigma: ", num2str(sigma_5));
-plot(coluna1(330:450), f_5, ':', 'LineWidth', 3, 'DisplayName',lgd_05);
+plot(coluna1(330:450), f_5, ':m', 'LineWidth', 3, 'DisplayName',lgd_05);
 hold off
 
 lgd = legend;
+
+hold off
